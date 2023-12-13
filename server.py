d5e38d02201dc209cc19f2bacd77b23d4f8fec43
Alexey <112414906+Alexey777F@users.noreply.github.com>
2023-12-14 01:41:09 +0300

    Add download file router

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/add_file', methods=['POST' ,"GET"])
+def upload_file():
+    if request.method == "GET":
+        return "HELLO"
+    file = request.files['file']
+    file.save('uploaded_image.png')
+    return 'File uploaded successfully'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9990)
