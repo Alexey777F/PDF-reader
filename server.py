f6d2b3bbe9b2cbd90207a7d9a63036fb81ff7008
Alexey <112414906+Alexey777F@users.noreply.github.com>
2023-12-14 02:22:04 +0300

    Add router get_files to save file in png images

--- server.py
+++ server.py
@@ -1,15 +1,29 @@
 from flask import Flask, request
+import os
+import fitz
 
 app = Flask(__name__)
+dir_name = "photos"
 
-@app.route('/add_file', methods=['POST' ,"GET"])
+@app.route('/add_file', methods=['POST' ,'GET'])
 def upload_file():
-    if request.method == "GET":
+    if request.method == 'GET':
         return "HELLO"
     file = request.files['file']
-    file.save('uploaded_image.pdf')
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    file.save(os.path.join(dir_name, 'uploaded_file.pdf'))
     return 'File uploaded successfully'
 
+@app.route('/get_files', methods=['POST', 'GET'])
+def convert_to_image():
+    with fitz.open(os.path.join(dir_name, 'uploaded_file.pdf')) as doc:
+        total_pages = len(doc)
+        for i in range(len(doc)):
+            page = doc.load_page(i)
+            pix = page.get_pixmap(dpi=110)
+            output = f'outfile_{i + 1}.png'
+            pix.save(os.path.join(dir_name, output))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9990)
