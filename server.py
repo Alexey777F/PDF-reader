672daf2c94133a9cd2b3c819a4dccd64580b88c8
Alexey <112414906+Alexey777F@users.noreply.github.com>
2023-12-14 14:53:51 +0300

    Modificate file with class ServerBack

--- server.py
+++ server.py
@@ -3,10 +3,31 @@ import os
 import fitz
 import shutil
 
-app = Flask(__name__)
-dir_name = "photos"
+class ServerBack():
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.dir_name = "photos"
+        self.total_pages = 0
 
-@app.route('/add_file', methods=['POST' ,'GET'])
+
+    def convert_to_image(self):
+        if os.path.exists(self.dir_name):
+            with fitz.open(os.path.join(self.dir_name, 'uploaded_file.pdf')) as doc:
+                total_pages_file = len(doc)
+                self.total_pages = total_pages_file
+                for i in range(len(doc)):
+                    page = doc.load_page(i)
+                    pix = page.get_pixmap(dpi=110)
+                    output = f'outfile_{i + 1}.png'
+                    pix.save(os.path.join(self.dir_name, output))
+
+
+create_app = ServerBack()
+app = create_app.app
+dir_name = create_app.dir_name
+
+
+@app.route('/add_file', methods=['POST', 'GET'])
 def upload_file():
     if request.method == 'GET':
         return "Wait for add files"
@@ -15,26 +36,8 @@ def upload_file():
         if not os.path.exists(dir_name):
             os.mkdir(dir_name)
         file.save(os.path.join(dir_name, 'uploaded_file.pdf'))
-        convert_to_image()
+        create_app.convert_to_image()
         return redirect(url_for('success_uploaded'))
-
-@app.route('/success_uploaded')
-def success_uploaded():
-    return 'File uploaded successfully'
-
-@app.route('/delete_success')
-def delete_success():
-    return 'Files deleted successfully'
-
-def convert_to_image():
-    if os.path.exists(dir_name):
-        with fitz.open(os.path.join(dir_name, 'uploaded_file.pdf')) as doc:
-            total_pages = len(doc)
-            for i in range(len(doc)):
-                page = doc.load_page(i)
-                pix = page.get_pixmap(dpi=110)
-                output = f'outfile_{i + 1}.png'
-                pix.save(os.path.join(dir_name, output))
 
 @app.route('/delete_files', methods=['DELETE', 'GET'])
 def delete_files():
@@ -44,6 +47,14 @@ def delete_files():
         if os.path.exists(dir_name):
             shutil.rmtree(dir_name)
             return redirect(url_for('delete_success'))
+
+@app.route('/success_uploaded')
+def success_uploaded():
+    return 'File uploaded successfully'
+
+@app.route('/delete_success')
+def delete_success():
+    return 'Files deleted successfully'
 
 
 if __name__ == '__main__':
